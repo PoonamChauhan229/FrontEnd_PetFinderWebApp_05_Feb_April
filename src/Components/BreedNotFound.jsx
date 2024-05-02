c500ee69f048a1447f45f1bdb68cf882b9248aca
PoonamChauhan229 <poonam.chauhan229@gmail.com>
2024-05-02 11:05:55 +0530

    final bugs fixation in process

--- src/Components/BreedNotFound.jsx
+++ src/Components/BreedNotFound.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
+import { FaExclamationTriangle } from 'react-icons/fa';
+
 
 const BreedNotFound = ({searchTerm}) => {
   return (
         <> 
-        <div className="container-fluid bg-primary py-5 mb-3 notFound-header" style={{height:"600px"}}>
-        <div className="container pt-1">
-            <div className="row justify-content-start">
-                <div className="col-lg-8 text-center text-lg-start">
-                    <h1 className="fw-bold text-capitalize mb-lg-4 ">No breeds found for {searchTerm}</h1>
-                    <h1 className="text-uppercase mb-lg-4 text-secondary text-capitalize">Try a Different Breed !!!</h1>
-                    <p className="fs-4 text-white mb-lg-4 text-primary">We'll notify you when it's available!</p>
-
-                </div>
-            </div>
+       <div className="container-fluid bg-primary py-4 mb-3 notFound-header" style={{ height: "400px" ,width:"96%"}}>
+      <div className="container">
+        <div className="row justify-content-start align-items-center">
+          <div className="col-lg-8 text-center">
+            <FaExclamationTriangle style={{ fontSize: '4rem', color: 'brown', marginBottom: '20px' }} />
+            <h1 className="fw-bold text-capitalize mb-4 text-white">No breeds found for "{searchTerm}"</h1>
+            <h2 className="text-capitalize mb-4 text-secondary text-success">Try a Different Breed !!!</h2>
+            <p className="fs-5 text-white mb-4">We're sorry, but the breed <span className="text-uppercase" style={{fontWeight:"bold",color:"brown"}}>"{searchTerm}"</span> is not currently available. Don't worry! We'll notify you as soon as it becomes available.</p>
+            <p className="fs-5 text-white mb-4">Meanwhile, feel free to explore other breeds or check back later.</p>
+         
+          </div>
         </div>
+      </div>
     </div>
         </>
       )
